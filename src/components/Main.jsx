7aadac17600818d391af67054602871aae62fcfd
stiven2k <144818273+stiven2k@users.noreply.github.com>
2024-11-27 08:56:10 -0500

    contenido de promociones

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -28,17 +28,17 @@ const Main = () => {
   ];
 
   const cleaningProducts = [
-    { id: 4, image: '/aseo1.png', name: 'FAB', price: 9.99 },
-    { id: 5, image: '/aseo2.png', name: 'Desinfectante', price: 14.99 },
-    { id: 6, image: '/aseo3.png', name: 'Jabón Líquido', price: 4.99 },
-    { id: 6, image: '/aseo4.png', name: 'Jabón Líquido', price: 4.99 },
+    { id: 4, image: 'https://www.fab.com.co/images/h0nadbhvm6m4/1n1TfWsbuJ7vWPOqNbJ4ax/e69c060077b1be2a4a33f8b27ed751bd/RkFCX19wb2x2b19VbHRyYV9GbGFzaF9WaWJyYV9Db2xvci5wbmc/1200w-1200h/fab-polvo-protecci%C3%B3n-color-pack-shot.jpg', name: 'FAB', price: 9.99 },
+    { id: 5, image: 'https://cdn1.totalcommerce.cloud/mercacentro/product-zoom/es/mr-musculo-antigrasa-verde-pistola-500-cm3-1.webp', name: 'Desinfectante', price: 14.99 },
+    { id: 6, image: 'https://www.tuhogar.com/content/dam/cp-sites/home-care/tu-hogar/latam/productos/axion/limon/thumbnail/axion-limon-product-1-4-l-render-mx.png', name: 'Jabón Líquido', price: 4.99 },
+    { id: 6, image: 'https://i5.walmartimages.com/seo/Fabuloso-All-Purpose-Cleaner-Passion-Fruit-16-9-fl-oz_2aa6ef21-a28d-4162-9a78-f23208d092eb_1.68131185635f012422886579e5e99135.png', name: 'Jabón para pisos', price: 4.99 },
   ];
 
   const fashionProducts = [
-    { id: 7, image: '/moda1.png', name: 'Vestido', price: 19.99 },
-    { id: 8, image: '/moda2.png', name: 'Blusa', price: 39.99 },
-    { id: 9, image: '/moda3.png', name: 'Bolso', price: 49.99 },
-    { id: 9, image: '/moda4.png', name: 'Botas', price: 49.99 },
+    { id: 7, image: 'https://scarsa.co/cdn/shop/files/ld9413fc-689.900.jpg?v=1702659282', name: 'Vestido', price: 19.99 },
+    { id: 8, image: 'https://lorella.com.co/wp-content/uploads/2024/06/Photoroom_20240605_174857-1-e1717628461283.png', name: 'Blusa', price: 39.99 },
+    { id: 9, image: 'https://belcorpcolombia.vtexassets.com/arquivos/ids/967434/210106116_fotofondoblanco1.jpg?v=638572674122570000', name: 'Bolso', price: 49.99 },
+    { id: 9, image: 'https://mercedescampuzano.vtexassets.com/arquivos/ids/208527/4845--BOTAS_NEGRO_BRITT1.jpg?v=638223491608870000', name: 'Botas', price: 49.99 },
   ];
 
   const nextSlide = () => {
--- src/pages/Promociones.jsx
+++ src/pages/Promociones.jsx
@@ -13,53 +13,86 @@ const Promociones = () => {
         titulo: "Descuento Navidad 1",
         descripcion: "¡Celebra la Navidad con un 20% de descuento en todos los productos navideños!",
         descuento: 20,
-        imagen: "https://via.placeholder.com/300x200?text=Navidad+1"
+        imagen: "https://img.freepik.com/fotos-premium/perfumes-nuevas-fragancias_900101-5269.jpg"
       },
       {
         id: 2,
         titulo: "Oferta Navidad 2",
         descripcion: "Compra 2 y lleva el tercero gratis en todos los adornos navideños.",
         descuento: 0, // No aplica porcentaje, sino una oferta
-        imagen: "https://via.placeholder.com/300x200?text=Navidad+2"
+        imagen: "https://www.samparfums.es/blog/wp-content/uploads/2021/09/perfumes-esencia-y-colonia-samparfums.jpg"
       },
       {
         id: 3,
         titulo: "Descuento Navidad 3",
         descripcion: "Disfruta de un 30% de descuento en nuestros paquetes de regalo.",
         descuento: 30,
-        imagen: "https://via.placeholder.com/300x200?text=Navidad+3"
+        imagen: "https://media.gq.com.mx/photos/5f03717b85180fb06783509c/16:9/w_2560%2Cc_limit/Cover.jpg"
       },
       {
         id: 4,
         titulo: "Oferta Especial Navidad",
         descripcion: "Hasta un 50% de descuento en productos seleccionados de Navidad.",
         descuento: 50,
-        imagen: "https://via.placeholder.com/300x200?text=Navidad+4"
+        imagen: "https://newsfragancias.com/wp-content/uploads/2020/03/carrusel-fragancias-primavera-definitivo.jpg"
       },
+      {
+        id: 5,
+        titulo: "Descuento Navidad 5",
+        descripcion: "Celebra la Navidad con un 40% de descuento en todos los productos navideños.",
+        descuento: 40,
+        imagen: "https://newsfragancias.com/wp-content/uploads/2020/07/Fragancias_femeninas_verano2.jpg"
+      }
     ];
 
     setPromociones(promocionesData);
   }, []);
 
   return (
-    <div>
-      <h4 >Promociones Navideñas</h4>
-      <div className="promociones-container">
-        {promociones.map((promo) => (
-          <div key={promo.id} className="promo-item">
-            <img src={promo.imagen} alt={promo.titulo} className="promo-img" />
-            <h2>{promo.titulo}</h2>
-            <p>{promo.descripcion}</p>
-            {promo.descuento > 0 ? (
-              <p><strong>Descuento:</strong> {promo.descuento}%</p>
-            ) : (
-              <p><strong>Oferta:</strong> {promo.descripcion}</p>
-            )}
-          </div>
-        ))}
-      </div>
-      <Footer /> 
-    </div>
+    <>
+      {/* Header Section */}
+     
+
+      {/* Introductory Section */}
+      <section className="intro">
+        <h2>¿Por qué tenemos las mejores promociones?</h2>
+        <p>
+        Celebra las fiestas con nuestras ofertas exclusivas en productos seleccionados. Descubre descuentos especiales en perfumes, adornos navideños y paquetes de regalo. Aprovecha nuestras promociones limitadas y encuentra el regalo perfecto para ti o tus seres queridos. ¡No dejes pasar estas increíbles ofertas navideñas!
+
+
+        </p>
+      </section>
+
+      {/* Products Section */}
+      <section>
+       
+        <div className="promociones-container">
+          {promociones.map((promo) => (
+            <div key={promo.id} className="promo-item">
+              <img src={promo.imagen} alt={promo.titulo} className="promo-img" />
+              <h2>{promo.titulo}</h2>
+              <p>{promo.descripcion}</p>
+              {promo.descuento > 0 ? (
+                <p><strong>Descuento:</strong> {promo.descuento}%</p>
+              ) : (
+                <p><strong>Oferta:</strong> {promo.descripcion}</p>
+              )}
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Testimonial Section */}
+      <section className="testimoniales">
+        <h2>Lo que nuestros clientes dicen</h2>
+        <div className="testimonial">
+          <p>"Nuestros clientes confirman que la calidad y la experiencia de compra son inigualables. Desde fragancias duraderas hasta un servicio de atención al cliente excepcional, muchos ya han encontrado su aroma perfecto. Aquí te compartimos lo que algunos de ellos tienen que decir sobre su experiencia de compra con nosotros. ¡Únete a nuestra comunidad de clientes felices y descubre tu fragancia ideal!"</p>
+          <span>- María P.</span>
+        </div>
+      </section>
+
+      <Footer />
+    </>
   );
 };
 
